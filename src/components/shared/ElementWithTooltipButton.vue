0393089e363e484cc39fc2625b4c671b2630d3ea
Ivan Novak <ispeakwithdead@gmail.com>
2026-02-24 01:59:16 +0100

    add tooltip UI

---
+++ src/components/shared/ElementWithTooltipButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="element-with-tooltip-button">
+    <slot />
+    <TooltipButton v-if="tooltipText" :text="tooltipText" class="ml-3" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TooltipButton from '@/components/shared/TooltipButton.vue';
+
+interface Props {
+  tooltipText: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  tooltipText: ''
+});
+</script>
+
+<style scoped>
+.element-with-tooltip-button {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+}
+
+.element-with-tooltip-button > :first-child {
+  flex: 1;
+}
+</style>
---
+++ src/components/shared/TooltipButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-btn flat round size="small" color="secondary" icon="question_mark" >
+    <q-tooltip class="text-body2">{{ text }}</q-tooltip>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  text: ''
+});
+</script>
+
+<style scoped>
+.tooltip-text {
+  font-size: 1.5rem;
+  font-weight: normal;
+  color: #fff;
+  padding: 1rem;
+  border-radius: 0.5rem;
+}
+</style>
